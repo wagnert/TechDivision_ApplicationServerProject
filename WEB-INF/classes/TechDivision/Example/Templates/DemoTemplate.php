0e343cf0d7e7a9b73376a766435e038c28bc512d
Tim Wagner <t.wagner@techdivision.com>
2014-03-11 17:52:37 +0100

    Refactor for servlet engine

--- WEB-INF/classes/TechDivision/Example/Templates/DemoTemplate.php
+++ WEB-INF/classes/TechDivision/Example/Templates/DemoTemplate.php
@@ -8,6 +8,17 @@
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
+ *
+ * PHP version 5
+ *
+ * @category   Application
+ * @package    TechDivision_ApplicationServerWebsite
+ * @subpackage Templates
+ * @author     Johann Zelger <jz@techdivision.com>
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/TechDivision_ApplicationServerWebsite
  */
 
 namespace TechDivision\Example\Templates;
@@ -15,13 +26,17 @@ namespace TechDivision\Example\Templates;
 use TechDivision\Example\Templates\AbstractTemplate;
 
 /**
- * @package     TechDivision\Example
- * @copyright   Copyright (c) 2013 <info@techdivision.com> - TechDivision GmbH
- * @license     http://opensource.org/licenses/osl-3.0.php
- *              Open Software License (OSL 3.0)
- * @author      Johann Zelger <jz@techdivision.com>
+ * Demo template.
+ * 
+ * @category   Application
+ * @package    TechDivision_ApplicationServerWebsite
+ * @subpackage Templates
+ * @author     Johann Zelger <jz@techdivision.com>
+ * @author     Tim Wagner <tw@techdivision.com>
+ * @copyright  2014 TechDivision GmbH <info@techdivision.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link       https://github.com/techdivision/TechDivision_ApplicationServerWebsite
  */
-
 class DemoTemplate extends AbstractTemplate
 {
 
@@ -98,6 +113,4 @@ class DemoTemplate extends AbstractTemplate
     {
         $this->baseUrl = $baseUrl;
     }
-
-
 }
